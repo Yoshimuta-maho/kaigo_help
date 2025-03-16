d641e53f141b50b0091d8df12e5db30b9fe056b8
EC2 Default User <ec2-user@ip-172-31-4-147.ap-northeast-1.compute.internal>
2025-03-16 10:28:31 +0000

    途中

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,19 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
 
+  def create
+    # name と password を使ってユーザーを検索
+    self.resource = User.find_by(name: params[:user][:name])
+
+    # name が見つかった場合、パスワードを照合してサインイン
+    if self.resource && self.resource.valid_password?(params[:user][:password])
+      sign_in(resource_name, self.resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      flash[:alert] = '名前かパスワードが間違っています。'
+      render :new
+    end
+  end
   # GET /resource/sign_in
   # def new
   #   super
@@ -21,10 +34,15 @@ class Users::SessionsController < Devise::SessionsController
   protected
   
   def after_sign_in_path_for(resource)
-    users_root_path  # ユーザーログイン後にユーザー専用トップへ遷移
+    user_path  # ユーザーログイン後にユーザー専用トップへ遷移
   end
 
   def after_sign_out_path_for(resource)
     new_user_session_path  # ログアウト後はログイン画面へ
+  end
+  private
+
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
   end
 end
