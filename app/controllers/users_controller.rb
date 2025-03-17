6134ebe181f850c0aac821d3b9c703a9f273dc6a
EC2 Default User <ec2-user@ip-172-31-4-147.ap-northeast-1.compute.internal>
2025-03-17 12:03:48 +0000

    実装フェーズ1完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:destroy]
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
@@ -7,11 +8,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+    @user = current_user # ここで現在のユーザーを直接設定
   end
 
   def update
-    @user = current_user
+    @user = current_user # ここでも現在のユーザーを取得
+    if user_params[:password].blank? && user_params[:password_confirmation].blank?
+      # パスワードが空の場合、パスワードの変更なしで更新
+      user_params.delete(:password)
+      user_params.delete(:password_confirmation)
+    end
+  
     if @user.update(user_params)
       redirect_to @user, notice: 'プロフィールが更新されました。'
     else
@@ -20,9 +27,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = current_user
+    @user = current_user # ここでも現在のユーザーを取得
     if @user.destroy
-      sign_out current_user  # 退会後にサインアウト
+      sign_out current_user 
       redirect_to root_path, notice: 'アカウントが削除されました。'
     else
       redirect_to @user, alert: 'アカウントの削除に失敗しました。'
@@ -31,7 +38,13 @@ class UsersController < ApplicationController
 
   private
 
+  def correct_user
+    if current_user != User.find(params[:id]) # 現在のユーザーとIDが一致しない場合リダイレクト
+      redirect_to root_path, alert: '他のユーザーのアカウントを操作することはできません。'
+    end
+  end
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image_icon)
+    params.require(:user).permit(:name, :email, :image_icon)
   end
 end
