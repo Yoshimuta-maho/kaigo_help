11bddece8ae2be8bae23fa8196574034fa34ea58
EC2 Default User <ec2-user@ip-172-31-4-147.ap-northeast-1.compute.internal>
2025-03-29 08:18:45 +0000

    controller修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,11 +1,9 @@
-module Admin
-  class HomesController < ApplicationController
-    before_action :authenticate_admin!
+class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
 
-    def top
-      # ダッシュボードの内容をここで定義
-      @total_users = User.count
-      @total_groups = Group.count
-    end
+  def top
+    # ダッシュボードの内容をここで定義
+    @total_users = User.count
+    @total_groups = Group.count
   end
 end
